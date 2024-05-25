0dd5c8cab9dd441fb240c8a5900dfeed7463c3b1
LourdenB15 <lourdenbaydal15@gmail.com>
2024-05-25 19:10:14 +0800

    Separate styles and scripts into independent files

---
+++ activities/bmi-calculator/script.js
@@ -0,0 +1,19 @@
+function calculateBMI() {
+  let weight = document.getElementById('weight').value;
+  let height = document.getElementById('height').value;
+  if (!height || !weight) return;
+  if (height <= 0 || weight <= 0) return;
+  height = height / 100;
+  let bmi = weight / (height * height);
+  let result = document.getElementById('result');
+  result.textContent = 'Your BMI is ' + bmi.toFixed(2);
+  if (bmi < 18.5) {
+    result.textContent += ' (Underweight)';
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    result.textContent += ' (Normal)';
+  } else if (bmi >= 25 && bmi < 29.9) {
+    result.textContent += ' (Overweight)';
+  } else {
+    result.textContent += ' (Obesity)';
+  }
+}
